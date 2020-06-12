5a59da6dd41844649bdd37a43d70b166010591d8
bcatoto <biancacatoto999@gmail.com>
2020-06-12 18:42:15 +0800

    added documentation

--- scraper.py
+++ scraper.py
@@ -2,11 +2,18 @@ import requests
 import json
 import os
 
+SPRINGER_NATURE_API_KEY = os.environ.get('SPRINGER_NATURE_API_KEY')
 SPRINGER_NATURE_FIELDS = ['title', 'creators', 'publicationNAME', 'issn',
     'eissn', 'doi', 'publisher', 'publicationDate', 'startingPage', 'endPage',
     'abstract']
 
-def springerUrlBuilder(s, subject, keyword, api):
+def springerUrlBuilder(s, subject, keyword):
+    """
+    Builds url to query Springer Nature API
+    :param s: start index of returned result
+    :param subject: subject constraint query
+    :param keyword: keyword constraint query
+    """
     # builds query
     query = 'type:Journal'
     if subject:
@@ -15,14 +22,19 @@ def springerUrlBuilder(s, subject, keyword, api):
         query += f'+keyword:\"{keyword}\"'
 
     # builds url
-    return f'http://api.springernature.com/meta/v2/json?s={s}&p=100&q=({query})&api_key={api}'
+    return f'http://api.springernature.com/meta/v2/json?s={s}&p=100&q=({query})&api_key={SPRINGER_NATURE_API_KEY}'
 
-def springerScraper(subject, keyword, api):
+def springerScraper(subject, keyword):
+    """
+    Scrapes metadata of all results returned from subject and keyword query
+    :param subject: subject constraint query
+    :param keyword: keyword constraint query
+    """
     i = 1
     total = 100
 
     while i <= total:
-        url = springerUrlBuilder(i, 'Food Science', 'flavor', api)
+        url = springerUrlBuilder(i, 'Food Science', 'flavor')
         response = requests.get(url)
         if response.ok:
             data = json.loads(response.content)
@@ -36,8 +48,7 @@ def springerScraper(subject, keyword, api):
         i += 100
 
 def main():
-    api = os.environ.get('SPRINGER_NATURE_API_KEY')
-    springerScraper('Food Science', 'flavor', api)
+    springerScraper('Food Science', None, SPRINGER_NATURE_API_KEY)
 
 if __name__ == '__main__':
     main()
