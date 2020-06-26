96c20198aba7be5f39db6087af55d9bda3694bc8
bcatoto <biancacatoto999@gmail.com>
2020-06-26 21:55:02 +0800

    removed validation

--- paiper/classifier/classifier.py
+++ paiper/classifier/classifier.py
@@ -3,7 +3,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from pymongo import MongoClient
 import pickle
-from sys import argv, stderr
 import os
 import random
 
@@ -13,21 +12,21 @@ MODELS_PATH = os.path.join(os.path.dirname(__file__), 'models')
 
 class Classifier:
 
-    def train_model(self, name, training_size=0.8, save_pickle=True, doc2vec=False):
+    def train_model(self, collection, training_size=0.8, save_pickle=True, doc2vec=False):
         """
         Trains Classifier based on set of relevant and irrelevant article abstracts
         (from MongoDB database)
         Features: preprocessed abstracts
         Values: relevant (1) or irrelevant (0)
 
-        :param name: name of MongoDB collection of articles to train model on
+        :param collection: name of MongoDB collection of articles to train model on
         :param training_size: percentage of articles to go in training set,
         remainder will go in testing set
         :param doc2vec: (not yet used) Bool flag to use doc2vec instead of tfidf
         """
         # queries database
         db = MongoClient(DATABASE_URL).classifier
-        articles = list(db[name].find())
+        articles = list(db[collection].find())
         random.shuffle(articles)
 
         training_abs = []
@@ -59,14 +58,14 @@ class Classifier:
         # scores model based on accuracy of testing set
         testing_pred = model.predict(testing_feat)
         score = model.score(testing_feat, testing_val)
-        print(f'{name} model accuracy: {score}')
+        print(f'{collection} model accuracy: {score}')
         print(classification_report(testing_val, testing_pred))
 
         # pickles vectorizer and model and saves to respective folders
         if save_pickle:
-            with open(os.path.join(VECTORIZERS_PATH, f'{name}_{round(score * 100)}_vectorizer.pkl'), 'wb') as file:
+            with open(os.path.join(VECTORIZERS_PATH, f'{collection}_{round(score * 100)}_vectorizer.pkl'), 'wb') as file:
                 pickle.dump(vectorizer, file)
-            with open(os.path.join(MODELS_PATH, f'{name}_{round(score * 100)}_model.pkl'), 'wb') as file:
+            with open(os.path.join(MODELS_PATH, f'{collection}_{round(score * 100)}_model.pkl'), 'wb') as file:
                 pickle.dump(model, file)
 
         self._vectorizer = vectorizer
@@ -88,17 +87,11 @@ class Classifier:
         with open(os.path.join(MODELS_PATH, model_name), 'rb') as file:
             self._model = pickle.load(file)
 
-    def predict(self, abs):
+    def predict(self, abstracts):
         """
         Vectorizes the abstracts (in list form) and returns predictions of model on features
         :param features: article abstracts to be classified as relevant or irrelevant
         based on model of Classifier
         """
-        if abs is None:
-            print("Error: No abstracts were provided", file=stderr)
-        if self._model is None:
-            print("Error: Classifier object does not contain a model", file=stderr)
-        
-        abs_feat = self._vectorizer.transform(abs)
-        return self._model.predict(abs_feat)
-        
+        features = self._vectorizer.transform(abstracts)
+        return self._model.predict(features)
