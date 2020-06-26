eaa8ec197d10871fed6bb209ad4d414a0e14f5d5
Rohan Jinturkar <Firebolt@Rohans-MacBook-Pro-2.local>
2020-06-26 18:13:06 -0400

    Added all scrapers to main_script.py

--- main_script.py
+++ main_script.py
@@ -1,6 +1,6 @@
 from paiper.loader import load_articles
 from paiper.classifier import Classifier
-from paiper.scraper import springer_scraper
+from paiper.scraper import springer_scraper, elsevier_scraper, pubmed_scraper
 
 # comment out lines that are not relevant on an as-needed basis
 # use this for debugging
@@ -10,7 +10,9 @@ def main():
     # classifier.train_model('matthew')
     classifier.load_vectorizer('matthew_98_vectorizer.pkl')
     classifier.load_model('matthew_98_model.pkl')
-    springer_scraper(classifier, keyword='extrusion')
+    # springer_scraper(classifier, keyword='animal feed')
+    # elsevier_scraper(classifier, query='flavor compounds')
+    pubmed_scraper(classifier, term='flavor compounds')
 
 if __name__ == '__main__':
     main()
