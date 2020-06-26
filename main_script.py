3cdee5bc161c4a12ebc9f9ed356495cdbb7342d8
bcatoto <biancacatoto999@gmail.com>
2020-06-26 22:04:52 +0800

    included scraper

--- main_script.py
+++ main_script.py
@@ -1,15 +1,16 @@
 from paiper.loader import load_articles
 from paiper.classifier import Classifier
+from paiper.scraper import springer_scraper
 
 # comment out lines that are not relevant on an as-needed basis
 # use this for debugging
 def main():
     # load_articles()
     classifier = Classifier()
-    classifier.train_model('matthew')
-    # classifier.load_vectorizer(vectorizer_name)
-    # classifier.load_model(model_name)
-    # classifier.predict(abstracts)
+    # classifier.train_model('matthew')
+    classifier.load_vectorizer('matthew_98_vectorizer.pkl')
+    classifier.load_model('matthew_98_model.pkl')
+    springer_scraper(classifier, keyword='extrusion')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
