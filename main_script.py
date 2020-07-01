409011c00df2532b2856a218b70920679e2cc041
bcatoto <biancacatoto999@gmail.com>
2020-07-01 14:55:06 +0800

    Added argument parser

--- main_script.py
+++ main_script.py
@@ -3,22 +3,26 @@ from paiper.classifier import Classifier
 from paiper.scraper.elsevier import ElsevierScraper
 from paiper.scraper.springer import SpringerScraper
 from paiper.scraper.pubmed import PubmedScraper
+import argparse
 import sys
 
-# comment out lines that are not relevant on an as-needed basis
-# use this for debugging
 def main():
-    # load_articles()
+    parser = argparse.ArgumentParser(description='Scrape abstracts')
+    parser.add_argument('query', type=str, help='database query')
+    parser.add_argument('--subject', type=str, default='', help='Springer Nature subject query')
+    parser.add_argument('-a', '--all', action='store_true', help='queries all databases')
+    parser.add_argument('-s', '--springer', action='store_true', help='queries Springer Nature database')
+    parser.add_argument('-p', '--pubmed', action='store_true', help='queries PubMed database')
+    parser.add_argument('-e', '--elsevier', action='store_true', help='queries Elsevier database')
+    parser.add_argument('-l', '--load', action='store_true', help='loads training data into database')
+    args = parser.parse_args()
+
+    # LOADS TRAINING DATA
+    if args.load:
+        load_articles()
 
     # CLASSIFIER
-    # classifier = Classifier()
     classifiers = [Classifier(), Classifier()]
-
-    # EITHER LOAD GABBY OR MATTHEW
-    # classifier.load_vectorizer('gabby_vectorizer.pkl')
-    # classifier.load_model('gabby_model.pkl')
-    # classifier.load_vectorizer('matthew_vectorizer.pkl')
-    # classifier.load_model('matthew_model.pkl')
     classifiers[0].load_vectorizer('gabby_vectorizer.pkl')
     classifiers[0].load_model('gabby_model.pkl')
     classifiers[1].load_vectorizer('matthew_vectorizer.pkl')
@@ -27,21 +31,20 @@ def main():
     # TAGS
     tags = ['gabby', 'matthew']
 
-    # QUERY: TYPE AS COMMAND LINE ARGUMENT (e.g: pipenv run python main_script.py food science)
-    query = ' '.join(sys.argv[1:])
-    # query = 'fatty'
-
     # SPRINGER SCRAPER
-    springer = SpringerScraper(tags, classifiers)
-    springer.scrape(keyword=query)
+    if args.all or args.springer:
+        springer = SpringerScraper(tags, classifiers)
+        springer.scrape(keyword=args.query)
 
     # PUBMED SCRAPER
-    pubmed = PubmedScraper(tags, classifiers)
-    pubmed.scrape(query)
+    if args.all or args.pubmed:
+        pubmed = PubmedScraper(tags, classifiers)
+        pubmed.scrape(args.query)
 
     # ELSEVIER SCRAPER
-    elsevier = ElsevierScraper(tags, classifiers)
-    elsevier.scrape(query)
+    if args.all or args.elsevier:
+        elsevier = ElsevierScraper(tags, classifiers)
+        elsevier.scrape(args.query)
 
 if __name__ == '__main__':
     main()
