02fd095063f5f50db56a5c14dd13566866faeabb
Rohan Jinturkar <rjintu@users.noreply.github.com>
2020-06-30 22:01:43 -0400

    Began doc2vec for classifier.py

--- paiper/classifier/classifier.py
+++ paiper/classifier/classifier.py
@@ -3,9 +3,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn import model_selection
 from pymongo import MongoClient
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import pickle
 import os
 import random
+import multiprocessing
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'Database url doesn\'t exist')
 VECTORIZERS_PATH = os.path.join(os.path.dirname(__file__), 'vectorizers')
@@ -17,7 +19,7 @@ class Classifier:
         """
         Trains Classifier based on set of relevant and irrelevant article abstracts
         (from MongoDB database)
-        Features: preprocessed abstracts
+        Features: preprocessed abstracts (in vector form)
         Values: relevant (1) or irrelevant (0)
 
         :param collection: name of MongoDB collection of articles to train model on
@@ -43,9 +45,32 @@ class Classifier:
         train_abs, test_abs, train_val, test_val = model_selection.train_test_split(abstracts, values, train_size=training_size, random_state=random_state)
 
         # vectorize abstracts
-        vectorizer = TfidfVectorizer()
-        train_feat = vectorizer.fit_transform(train_abs)
-        test_feat = vectorizer.transform(test_abs)
+        # check for doc2vec option
+        if doc2vec:
+            #fill up doc2vec over here
+            # https://towardsdatascience.com/implementing-multi-class-text-classification-with-doc2vec-df7c3812824d
+            # https://radimrehurek.com/gensim/models/doc2vec.html
+
+            print('Testing doc2vec: to be completed')
+            print('This part currently does not function properly')
+            train_documents = []
+            test_documents = []
+            for i, train_abstract in enumerate(train_abs):
+                train_documents.append(TaggedDocument(words=train_abstract.split(), tags=train_val[i]))
+            for j, test_abstract in enumerate(test_abstract):
+                test_documents.append(TaggedDocument(words=test_abstract.split(), tags=test_val[i]))
+            cores = multiprocessing.cpu_count()
+            model_d2v = Doc2Vec(dm=0, vector_size=300, negative=5, hs=0, min_count=2, workers=cores, alpha=0.025, min_alpha=0.001)
+            model_d2v.build_vocab([x for x in train_documents])
+            train_documents = random.shuffle(train_documents, random_state=random_state)
+            model_d2v.train(train_documents,total_examples=len(train_documents), epochs=30)
+
+        
+        # default to tf-idf
+        else:
+            vectorizer = TfidfVectorizer()
+            train_feat = vectorizer.fit_transform(train_abs)
+            test_feat = vectorizer.transform(test_abs)
 
         # train model
         model = LogisticRegression()
