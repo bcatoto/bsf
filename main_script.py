958631a298f0523e43dca0a657cc8e3309e51979
bcatoto <biancacatoto999@gmail.com>
2020-07-07 17:18:18 +0800

    Added feature to save all abstracts under general tag

--- main_script.py
+++ main_script.py
@@ -13,17 +13,19 @@ def main():
     # SET UP PARSER
     parser = argparse.ArgumentParser(description='Scrape abstracts')
     parser.add_argument('-l', '--load', action='store_true', help='loads training data into database')
-    parser.add_argument('-c', '--classifier', action='store_true', help='initialize classifiers')
-    parser.add_argument('-f', '--food2vec', action='store_true', help='initialize word2vec models')
-    parser.add_argument('-t', '--train', action='store_true', help='train models (Classifier or Food2Vec)')
+    parser.add_argument('-c', '--classifier', action='store_true', help='trains classifiers')
     parser.add_argument('-k', '--keywords', action='store_true', help='opens keywords.txt and scrapes for each keyword, one at a time')
     parser.add_argument('--query', type=str, default='', help='database query (requires quotation marks)')
     parser.add_argument('--subject', type=str, default='', help='Springer Nature subject query (requires quotation marks)')
     parser.add_argument('--collection', type=str, default='all', help='collection to store scraped abstracts in')
+    parser.add_argument('-o', '--store', action='store_true', help='stores all scraped abstracts in general tag')
     parser.add_argument('-a', '--all', action='store_true', help='scrapes all databases')
     parser.add_argument('-s', '--springer', action='store_true', help='scrapes Springer Nature database')
     parser.add_argument('-p', '--pubmed', action='store_true', help='scrapes PubMed database')
     parser.add_argument('-e', '--elsevier', action='store_true', help='scrapes Elsevier database')
+    parser.add_argument('-f', '--food2vec', action='store_true', help='initializes word2vec models')
+    parser.add_argument('-t', '--train', action='store_true', help='trains word2vec models')
+
     args = parser.parse_args()
 
     # LOAD TRAINING DATA
@@ -31,14 +33,13 @@ def main():
         load_articles()
 
     # CLASSIFIER
-    if args.classifier:
-        classifiers = [Classifier('gabby'), Classifier('matthew')]
-        for classifier in classifiers:
-            if args.train:
-                classifier.train_model()
-            else:
-                classifier.load_vectorizer()
-                classifier.load_model()
+    classifiers = [Classifier('gabby'), Classifier('matthew')]
+    for classifier in classifiers:
+        if args.classifier:
+            classifier.train_model()
+        else:
+            classifier.load_vectorizer()
+            classifier.load_model()
 
     # USE ALL SCRAPERS
     if args.all:
@@ -52,30 +53,30 @@ def main():
             keywords = [word.strip() for word in queries]
         for keyword in keywords:
             if args.springer:
-                springer = SpringerScraper(classifiers, collection=args.collection)
+                springer = SpringerScraper(classifiers, collection=args.collection, save_all=args.store)
                 springer.scrape(subject=args.subject, keyword=keyword)
             if args.pubmed:
-                pubmed = PubmedScraper(classifiers, collection=args.collection)
+                pubmed = PubmedScraper(classifiers, collection=args.collection, save_all=args.store)
                 pubmed.scrape(keyword)
             if args.elsevier:
-                elsevier = ElsevierScraper(classifiers, collection=args.collection)
+                elsevier = ElsevierScraper(classifiers, collection=args.collection, save_all=args.store)
                 elsevier.scrape(keyword)
 
     # USE QUERY FROM COMMAND LINE
     else:
         # SPRINGER SCRAPER
         if args.springer:
-            springer = SpringerScraper(classifiers, collection=args.collection)
+            springer = SpringerScraper(classifiers, collection=args.collection, save_all=args.store)
             springer.scrape(subject=args.subject, keyword=args.query)
 
         # PUBMED SCRAPER
         if args.pubmed:
-            pubmed = PubmedScraper(classifiers, collection=args.collection)
+            pubmed = PubmedScraper(classifiers, collection=args.collection, save_all=args.store)
             pubmed.scrape(args.query)
 
         # ELSEVIER SCRAPER
         if args.elsevier:
-            elsevier = ElsevierScraper(classifiers, collection=args.collection)
+            elsevier = ElsevierScraper(classifiers, collection=args.collection, save_all=args.store)
             elsevier.scrape(args.query)
 
     # RUN WORD2VEC
--- paiper/scraper/scraper.py
+++ paiper/scraper/scraper.py
@@ -12,7 +12,7 @@ class Scraper:
     nlp = spacy.load('en_core_web_sm')
     processor = MaterialsTextProcessor()
 
-    def __init__(self, classifiers, collection = 'all'):
+    def __init__(self, classifiers, collection='all', save_all=False, gen_tag='food science'):
         """
         Initializes classifiers and collection
         :param classifiers: model to determine relevance of abstract
@@ -20,6 +20,8 @@ class Scraper:
         """
         self._classifiers = classifiers
         self._collection = self.db[collection]
+        self._save = save_all
+        self._gen_tag = gen_tag
         print(f'Collection: {collection}')
 
     def _get_id(self, data, key):
@@ -27,6 +29,70 @@ class Scraper:
             return data[key]
         except KeyError:
             return None
+
+    def _save_all(self, articles, doi):
+        """
+        Stores all articles from database query under general tag
+        :param articles: list of article objects to add to database
+        :param abstracts: list of processed abstracts to be checked against classifier
+        :param doi: Bool flag for whether stored IDs are DOI
+        """
+        total = len(articles)
+
+        # progress bar
+        bar = ChargingBar(f'Storing all new papers to \'{self._gen_tag}\':', max=total, suffix='%(index)d of %(max)d')
+
+        requests = []
+
+        # creates request to stor article with corresponding tag
+        for article in articles:
+            id = article['doi'] if doi else article['uid']
+
+            # inserts new document if it does not exist
+            requests.append(UpdateOne(
+                { 'doi' if doi else 'uid': id, },
+                {
+                    '$setOnInsert': {
+                        'doi': article['doi'],
+                        'uid': article['uid'],
+                        'title': article['title'],
+                        'abstract': article['abstract'],
+                        'url': article['url'],
+                        'creators': article['creators'],
+                        'publication_name': article['publication_name'],
+                        'issn': article['issn'],
+                        'eissn': article['eissn'],
+                        'publication_date': article['publication_date'],
+                        'database': article['database'],
+                        'processed_abstract': article['processed_abstract'],
+                        'tags': [self._gen_tag]
+                    },
+                },
+                upsert=True
+            ))
+
+            # modifies existing document to include tag
+            requests.append(UpdateOne(
+                {
+                    'doi' if doi else 'uid': id,
+                    'tags': { '$ne' : self._gen_tag }
+                },
+                { '$push': { 'tags': self._gen_tag } }
+            ))
+
+            bar.next()
+        bar.finish()
+
+        # updates database
+        print(f'Updating collection...')
+        if requests:
+            mongo = self._collection.bulk_write(requests)
+
+        # calculates how many new relevant articles were added
+        new = mongo.upserted_count + mongo.modified_count if mongo else 0
+
+        print(f'Total articles analyzed: {total}.')
+        print(f'Stored {new} new abstracts to \'{self._gen_tag}\'.')
 
     def _store(self, articles, abstracts, doi=True):
         """
@@ -107,6 +173,9 @@ class Scraper:
             print(f'Ignored {irrelevant} abstracts irrelevant to \'{classifier.tag}\'.')
             print(f'Ignored {total - relevant - irrelevant} articles already tagged as \'{classifier.tag}\'.')
             print()
+
+        if self._save:
+            self._save_all(articles, doi)
 
     def set_classifiers(self, classifiers):
         """
