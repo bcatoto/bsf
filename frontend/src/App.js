252e9872f1906aaec2913d999749b2ee44d07655
bcatoto <biancacatoto999@gmail.com>
2020-07-27 18:03:26 +0800

    Created query text form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+import "bootswatch/dist/litera/bootstrap.min.css";
+import "./App.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: null
+    };
+  }
+
+  handleChange = event => {
+    const query = event.target.value;
+    this.setState({ query });
+    console.log(this.state);
+  }
+
+  handleSubmit() {
+    console.log("Submit: " + this.state);
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <Container>
+          <Form onSubmit={this.handleSubmit}>
+              <Form.Group as={Row}>
+                <Form.Label column sm={2} className="col-form-label-lg">Query</Form.Label>
+                <Col>
+                  <Form.Control className="form-control-lg"
+                    type="text" name="query"
+                    placeholder="Flavor"
+                    onChange={this.handleChange}
+                  />
+                </Col>
+                <Col sm={2}>
+                  <Button type="submit" variant="primary">
+                    Submit
+                  </Button>
+                </Col>
+              </Form.Group>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
