4012941cbe52022e8e334d5f7937b18aa82bd901
Rohan Jinturkar <rjintu@users.noreply.github.com>
2020-09-05 20:31:09 -0400

    Add lambda function

---
+++ backend/lambda_function.py
@@ -0,0 +1,43 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    term = event['term'].lower().replace(' ', '_')
+    
+    query = {}
+    query['topn'] = event['topn']
+    query['positive'] = [term]
+    query['negative'] = []
+    
+    for vector in event['vectors']:
+        term = vector['term'].lower().replace(' ', '_')
+        if vector['positive']:
+            query['positive'].append(term)
+        else:
+            query['negative'].append(term)
+    
+    payload = json.dumps(query).encode('utf-8')
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='application/json',
+                                       Body=payload)
+                                       
+    results = json.loads(response['Body'].read().decode())
+    
+    results['query'] = event['term']
+    for vector in event['vectors']:
+        if vector['positive']:
+            results['query'] += ' + ' + vector['term']
+        else:
+            results['query'] += ' - ' + vector['term']
+            
+    for result in results['results']:
+        result['term'] = result['term'].replace('_-_', '-').replace('_', ' ')
+    
+    return results
